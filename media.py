f98c7cc9fedb732c007d60b911edfa05a5d96a12
Hans Li <hans.li@richmond.edu>
2015-07-06 10:00:42 -0700

    Updated with a space after the hash sign

--- media.py
+++ media.py
@@ -1,10 +1,10 @@
-#importing dependencies
+# Importing dependencies
 import webbrowser
 
-#This movie class contains information regarding the movie.
+# This movie class contains information regarding the movie.
 class Movie():
 
-    #This assign the movie properties to self.
+    # This assign the movie properties to self.
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, main_actor, release_date):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -13,6 +13,6 @@ class Movie():
         self.main_actor = main_actor
         self.release_date = release_date
         
-    #this instance method allows you to show the trailer when called.
+    # This instance method allows you to show the trailer when called.
     def show_trailer (self):
         webbrowser.open(self.trailer_youtube_url)
